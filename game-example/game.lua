a440a786295b9fb616e6cd35eb5fa6afdd64626b
Marcelo Junior <marcelo.jr63@gmail.com>
2026-01-04 13:48:20 -0300

    Update game-example

--- game-example/game.lua
+++ game-example/game.lua
@@ -13,23 +13,24 @@ for i = 1, #Palette do
 end
 
 function update()
-    x = x + 4
-    t = t + 0.05
+    -- x = x + 4
+    -- t = t + 0.05
 
-    y =  math.sin(t) * 25
+    -- y =  math.sin(t) * 25
 
-    ui.draw_text("Bem-vindo ao Lupi!", 280, 180 + math.floor(y))
+    -- ui.draw_text("Bem-vindo ao Lupi!", 280, 180 + math.floor(y))
 
-    -- ui.draw_line(10, 10, 280, 180, 3)
+    -- ui.draw_line(10, 10, 280, 180, 9)
     -- ui.draw_rect(50, 50, 80, 80, true, 2)
 
     -- ui.draw_rect(185, 185, 50, 50, false, 5)
-    -- ui.draw_circle(200, 300, 20, true, 6, true, 10)
-    -- ui.draw_circle(260, 300, 20, true, 7, false, 11)
+    -- ui.draw_circle(50, 90, 20, true, 6, true, 2)
+    -- ui.draw_circle(30, 60, 20, true, 8, false, 8)
     -- ui.draw_circle(200, 360, 20, false, 8, true, 12)
     -- ui.draw_triangle(20, 250, 100, 250, 55, 350, 9)
 
     ui.spr(SpriteSheets['tilemap.leaves'], 10, 50)
+
 
 
     ui.tile(SpriteSheets['player.run.' .. frame + 1], 0, player_x, player_y)
