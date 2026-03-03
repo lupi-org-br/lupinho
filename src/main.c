ff47c42c33504b2cbd6b9a3c75174fdb5a7eaae1
Marcelo Junior <marcelo.jr63@gmail.com>
2026-03-02 21:14:43 -0300

    Add input file .lupi input

--- src/main.c
+++ src/main.c
@@ -9,7 +9,14 @@
     #include <emscripten/emscripten.h>
 #endif
 
+#if defined(PLATFORM_WEB)
+static volatile int game_is_loading = 0;
+#endif
+
 void UpdateDrawFrame() {
+#if defined(PLATFORM_WEB)
+    if (game_is_loading) return;
+#endif
     lua_api_call_update();
 
     BeginDrawing();
@@ -27,12 +34,22 @@ void UpdateDrawFrame() {
     clear_frame_buffer();
 }
 
+#if defined(PLATFORM_WEB)
+EMSCRIPTEN_KEEPALIVE
+void lupi_load_game(const char *game_dir) {
+    game_is_loading = 1;
+    lua_api_close();
+    lua_api_init();
+    reset_ui_state();
+    lua_api_setup_game(game_dir);
+    game_is_loading = 0;
+}
+#endif
+
 int main() {
     lua_api_init();
 
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Lupi Emulator");
-
-    lua_api_setup_game("game-example");
 
     SetTargetFPS(60);
 
